208244fcca3ddaf0c1ec58639ebcc1e93a22919e
shahariar270 <mdshahariar102@gmail.com>
2025-12-26 22:06:37 +0600

    secondary color apply

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         videoId='2IFDMvfJJHc'
       /> */}
       <DashData />
-      <Button label="Click Me" onClickHandle={hello} />
+      <Button label="Click Me" onClickHandle={hello} variant={'secondary'} />
       {open &&
         <>
           <Modal
