4945d690d70f4cfee342bc46ff51a961c70a1e38
shahariar270 <mdshahariar102@gmail.com>
2025-12-26 11:44:14 +0600

    wrap notification hooks

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './assets/styles/global.scss'
+import { NotificationProvider } from './components/Notifications'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
